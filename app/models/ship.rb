f5e635f4ea4caad848fc7a137b3e789f3b112709
EvanTheSmith <ProfessionalEmail@zoho.com>
2020-06-20 14:24:25 -0500

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+    attr_accessor :name, :type, :booty
+    @@all = []
+
+    def initialize(deets)
+        @name = deets["name"]
+        @type = deets["type"]
+        @booty = deets["booty"]
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        @@all.clear
+    end
 end

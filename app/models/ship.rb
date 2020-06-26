52b7571f5b8c40e1970e1912101b54fe9c09a446
Vlad <VladDaImpaler@gmail.com>
2020-06-26 19:51:35 -0400

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,17 @@
 class Ship
+    attr_accessor :name, :type, :booty
+    @@all = []
+
+    def initialize(args)
+        args.each { |key, value| self.send("#{key}=", value)}
+        @@all<<self
+    end
+    
+    def self.all
+        @@all
+    end
+    
+    def self.clear
+        @@all.clear
+    end
 end

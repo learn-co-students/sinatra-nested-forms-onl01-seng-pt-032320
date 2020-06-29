33f80381bef913e1769e3192b80ab3e00e5a70c1
Caroline <cvrieous@gmail.com>
2020-06-29 10:02:09 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,9 +1,23 @@
+# frozen_string_literal: true
+
 require './environment'
 
 module FormsLab
   class App < Sinatra::Base
+    get '/' do
+      erb :root
+    end
 
-    # code other routes/actions here
+    get '/new' do
+      erb :'pirates/new'
+    end
 
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+      @ship1 = Ship.new(params[:pirate][:ships][0])
+      @ship2 = Ship.new(params[:pirate][:ships][1])
+
+      erb :'pirates/show'
+    end
   end
 end

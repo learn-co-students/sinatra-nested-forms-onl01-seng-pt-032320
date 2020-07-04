748a8caef4e5db0c9169339de16259a7523f339a
Bryce B <bryce.boddy@gmail.com>
2020-07-04 04:51:54 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
-class Ship 
+class Ship < Pirate
+  attr_accessor :name, :type, :booty
+  @@all = []
+  def initialize(params)
+    @name = params[:ship_name_1]
+    @type = params[:ship_type_1]
+    @booty = params[:ship_booty_1]
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end 
+  
+  def self.clear
+    @@all = []
+    @@all
+  end
 end

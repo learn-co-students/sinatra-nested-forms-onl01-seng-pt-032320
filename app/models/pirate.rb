ba5721fe703f5c107dd5e80dff941290d5f0445e
arnie b <arnold.bwaila@gmail.com>
2020-07-15 13:26:54 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,14 @@
 class Pirate
+  attr_accessor :name, :type, :height
+  @@all = []
+  def initialize(properties)
+    name = properties[:name]
+    type = properties[:type]
+    height = properties[:height]
+    @@all << self 
+  end
+  
+  def self.all
+    @@all 
+  end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,14 @@
 class Ship
+  attr_accessor :name, :type, :booty 
+  @@all = []
+  def initialize(properties)
+    name = properties[:name]
+    type = properties[:type]
+    booty = properties[:booty]
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all 
+  end
 end
